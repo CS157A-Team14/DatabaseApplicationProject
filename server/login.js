b90dfef4d39d62e27481a3994a0fc413f3ea5060
Khoa phan <43330356+khoaphann@users.noreply.github.com>
2019-11-14 14:56:49 -0800

    minor change

--- server/login.js
+++ server/login.js
@@ -2,10 +2,11 @@ var express = require("express")
 var router = express.Router()
 
 router.get('/', (request ,response , next) => {
-    connection.query('SELECT * FROM product', function(err, result){
-      if(err) throw err
-      response.status(200).jsonp(result)
-    })
+    // connection.query('SELECT * FROM product', function(err, result){
+    //   if(err) throw err
+    //   response.status(200).jsonp(result)
+    // })
+    response.status(200).jsonp('Co1nnected')
   })
 
   module.exports = router
